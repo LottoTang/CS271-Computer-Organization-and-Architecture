f69d77667d8ee94f8955ab16e00c979c6e9fd7f8
LottoTang <107705303+LottoTang@users.noreply.github.com>
2022-07-15 00:17:17 +0800

    Add files via upload
    5th commit

--- Project3/Proj3_tanglon.asm
+++ Project3/Proj3_tanglon.asm
@@ -20,6 +20,9 @@ RANGE1_UPPER = -100
 RANGE2_LOWER = -50
 RANGE2_UPPER = -1
 
+; for part 4
+ROUNDUPVAL = 5
+
 .data
 
 ; (insert variable definitions here)
@@ -30,7 +33,7 @@ intro1		BYTE	"We will be accumulating user-input negative integers between the s
 intro2		BYTE	"statistics of the input values including minimum, maximum, and average values values, total sum, and" ,  0
 intro3		BYTE	"total number of valid inputs." ,  0
 prompt1		BYTE	"What is your name? ",  0
-userName	BYTE	21 DUP (0)						; variable for asking userName; set the size as 20 (1 extra for null-terminated) as name should within 20 characters
+userName	BYTE	21 DUP (0)						; variable for asking userName, set the size as 20 (1 extra for null-terminated) as name should within 20 characters
 greeting2	BYTE	"Hello there, ",  0
 
 ; for part 2
@@ -40,12 +43,12 @@ intro5		BYTE	"] or [",  0
 intro6		BYTE	"].",  0
 intro7      BYTE    "Enter a non-negative number when you are finished to see results.",  0
 prompt2     BYTE    " - Enter number: ",  0
-extracd1    BYTE    "EC: Number the lines during user input"
+extracd1    BYTE    "EC: Number the lines during user input",  0
 extracd2    BYTE    "Line ",  0
 
-countLine   BYTE    1
-counter     BYTE    0                               ; store how many numbers
-value       SWORD   ?                               ; signed variable to store the negative value; results...
+countLine   DWORD    1
+counter     DWORD    0                              ; store how many numbers
+value       SDWORD   ?                              ; signed variable to store the negative value; results...
 maxVal		SDWORD	-201                            ; initialize as the lowest possible value - 1
 minVAL		SDWORD	0                               ; initialize as the largest possible value + 1
 sum			SDWORD	0
@@ -55,6 +58,9 @@ average		SDWORD	?
 warning1    BYTE    "Number Invalid!",  0
 
 ; for part 3
+quotient	DWORD	?
+remainder	DWORD	?
+tenth		DWORD	-10
 warning2    BYTE    "No numbers entered!",  0
 outcome1    BYTE    "You have entered ",  0
 outcome2    BYTE    " valid numbers.",  0
@@ -132,10 +138,14 @@ main PROC
     MOV		EDX,  OFFSET  intro7
 	CALL	WriteString
 	CALL	CrLF
+	CALL	CrLF
+	MOV     EDX,  OFFSET  extracd1                  ; prompt for extra credit
+    CALL	WriteString
+	CALL	CrLF
 
 ; ask for user input; jump back here if non-zero is entered
 _userInput:
-    MOV     EDX,  OFFSET  extracd1                  ; prompt for extra credit
+	MOV     EDX,  OFFSET  extracd2                  ; prompt for extra credit
     CALL	WriteString
     MOV     EAX,  countLine
     CALL    WriteDec
@@ -164,10 +174,12 @@ _invalid:
 _again:
     INC     countLine
     INC     counter                                 ; increment counter to count number of values entered
-    ADD     sum,  value
-    CMP     value,  maxVal
-    JG      changeMax                               ; check max & min value
-    JMP     checkMin
+	MOV		EAX,  value
+    ADD     sum,  EAX
+	MOV		EAX,  value								; re-initialize EAX to prevent any error
+    CMP     EAX,  maxVal
+    JG      _changeMax                              ; check max & min value
+    JMP     _checkMin
 
 
 ; jump here if the entered value is invalid (greater than -100)
@@ -181,20 +193,23 @@ _checkRange2Lower:
 
 ; update max value
 _changeMax:
-    MOV     maxVal,  value
-    JMP     checkMin
+    MOV		EAX,  value
+	MOV     maxVal,  EAX
+    JMP     _checkMin
 
 
 ; check min value
 _checkMin:
-    CMP     value,  minVal
+    MOV		EAX,  value
+	CMP     EAX,  minVal
     JL      _changeMin
     JMP     _userInput
 
 
 ; update min value
 _changeMin:
-    MOV     minVal,  value
+    MOV		EAX,  value
+	MOV     minVal,  EAX
     JMP     _userInput
 
 
@@ -209,18 +224,6 @@ _nonNegative:
 	;--------------------------------------
 	CMP     counter,  0
 	JZ      _noNumber
-
-    ;--------------------------------------
-	; This part is for calculating the average number
-	;	the average will be rounded up
-	;--------------------------------------
-
-    MOV     EAX,  sum                                ; move sum (dividend) into AX
-    CDD                                             ; sign-extend AX into DX
-    MOV     EBX,  counter                            ; move counter (divisor) into BX
-    IDIV    EBX
-    MOV     average,  EAX                            ; store quotient into AX
-    ADD     average,  EDX                            ; perform round up action
 
     ; for displaying the results
     MOV     EDX,  OFFSET  outcome1
@@ -249,13 +252,42 @@ _nonNegative:
     CALL    WriteInt
     CALL    CrLF
 
-    MOV     EDX,  OFFSET  outcome6
+	;--------------------------------------
+	; This part is for calculating the average number
+	;	the average will be rounded up
+	;--------------------------------------
+
+	MOV     EDX,  OFFSET  outcome6					; display the rounded average
     CALL    WriteString
-    MOV     EAX,  average                           ; display the rounded average
-    CALL    WriteInt
-    CALL    CrLF
+	
+	MOV     EAX,  sum                               ; move sum (dividend) into AX
+    CDQ                                             ; sign-extend AX into DX
+    MOV     EBX,  counter                           ; move counter (divisor) into BX
+    IDIV    EBX
+    MOV     quotient,  EAX                          ; store quotient into variable
+    MOV     remainder,  EDX                         ; store remainder into variable
+
+	MOV		EAX,  remainder
+	MUL		tenth
+	CDQ
+	MOV		EBX,  counter
+	DIV		EBX
+	CMP		EAX,  ROUNDUPVAL
+	JG		_roundUp
+	JMP		_noRoundUp
+
+_roundUp:											
+	MOV		EAX,  quotient
+	DEC		EAX
+	CALL	WriteInt
+	CALL    CrLF
     JMP     _bye
 
+_noRoundUp:
+	MOV		EAX,  quotient
+	CALL	WriteInt
+	CALL    CrLF
+    JMP     _bye
 
  ; display no numbers entered and jump to _bye
 _noNumber:
@@ -274,6 +306,7 @@ _bye:
 
 	Invoke ExitProcess,0	                        ; exit to operating system
 	;http://masm32.com/board/index.php?topic=1226.15
+	;https://github.com/cacarrilloc/ASSEMBLY-PROJECTS-MASM/blob/master/Integer_Accumulator.asm
 main ENDP
 
 ; (insert additional procedures here)
