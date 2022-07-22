52179c2e7df891a199c3017beb83bacebaf30887
LottoTang <107705303+LottoTang@users.noreply.github.com>
2022-07-23 01:22:02 +0800

    done basic

--- Project4/Proj4_tanglon.asm
+++ Project4/Proj4_tanglon.asm
@@ -17,6 +17,13 @@ INCLUDE Irvine32.inc
 LOWER_BOUND = 1
 UPPER_BOUND = 200
 MAX_ROW = 10
+TEN = 10
+HUNDRED = 100
+THOUSAND = 1000
+TENTHOUSAND = 10000
+
+; for extra credit
+UPPER_BOUND2 = 4000
 
 .data
 
@@ -36,15 +43,21 @@ prompt2     BYTE    " ... ",  0
 prompt3     BYTE    "]: ",  0
 
 ; for showPrimes
-output1     BYTE    "   ",  0
-rowCounter  BYTE    0
-value       DWORD    0
+output1     BYTE    "       ",  0
+output2     BYTE    "      ",  0
+output3     BYTE    "     ",  0
+output4     BYTE    "    ",  0
+wordCount   BYTE    0                                       ; count the number of displayed values
+value       DWORD   0                              
+extracd1    BYTE    " **EC: Align the output columns** ",  0
 
 ; for isPrime
-primeNum    DWORD    2                               ; divisor start with 2 and end with dividend - 1
+primeNum    DWORD    2                                      ; divisor start with 2 and end with dividend - 1
 
 ; for farewell
 bye         BYTE    "Results certified by Lotto. Goodbye.",  0
+bye1        BYTE    " ** End of Program - Prime Numbers in range 1 - 200** ",  0
+finished    BYTE    0                                       ; once the basic program finished, set as 1 to indicate start of program in range [1...4000]
 
 .code
 main PROC
@@ -55,11 +68,13 @@ main PROC
 
 	CALL    getUserData
 
-	CALL    isPrime
+	CALL    showPrimes
 
 	CALL    farewell
 
-	Invoke ExitProcess,0	                        ; exit to operating system
+    CALL    getUserData2
+
+	Invoke ExitProcess,  0                                  ; exit to operating system
 
 main ENDP
 
@@ -103,24 +118,24 @@ validate PROC
 ; returns: return the validated input from EAX to 'value'
 ;============================================
     ; check ranges
-    MOV     EBX,  0                                 ; initialize EBX as 0 (default as true)
-    CMP     EAX,  LOWER_BOUND                       ; user's input is stored in EAX from previous procedure
+    MOV     EBX,  0                                         ; initialize EBX as 0 (default as true)
+    CMP     EAX,  LOWER_BOUND                               ; user's input is stored in EAX from previous procedure
     JL      _error
     CMP     EAX,  UPPER_BOUND
     JG      _error
 
     ; validated input
-    MOV     value,  EAX                             ; EAX is now storing the user input
+    MOV     value,  EAX                                     ; EAX is now storing the user input
 	RET
 
 	; jump here if invalid data found
 	_error:
-	MOV     EDX,  OFFSET  error1                    ; display error message
+	MOV     EDX,  OFFSET  error1                            ; display error message
 	CALL    WriteString
 	CALL    CrLF
 	MOV     EBX,  1
     CALL    CrLF
-    RET                                             ; EBX as 1 to indicate error spotted
+    RET                                                     ; EBX as 1 to indicate error spotted
 
 validate ENDP
 
@@ -147,13 +162,14 @@ _askInput:
     MOV     EDX,  OFFSET  prompt3
     CALL    WriteString
 
-    MOV     EAX,  0                                 ; initialize EAX as 0
-    CALL    ReadInt                                 ; ask for user input
+    MOV     EAX,  0                                         ; initialize EAX as 0
+    CALL    ReadInt                                         ; ask for user input
+    CALL    CrLF
 
-    CALL    validate                                ; call validate to check if within range
-    CMP     EBX,  1                                 ; returned EBX: if 0 (valid input); 1 (invalid input)
-    JE      _askInput                               ; ask user's input again if EBX = 1
-    RET                                             ; validate already move EAX into 'value' already
+    CALL    validate                                        ; call validate to check if within range
+    CMP     EBX,  1                                         ; returned EBX: if 0 (valid input); 1 (invalid input)
+    JE      _askInput                                       ; ask user's input again if EBX = 1
+    RET                                                     ; validate already move EAX into 'value' already
 
 getUserData ENDP
 
@@ -167,37 +183,71 @@ showPrimes PROC
 ; returns: the formatted output for prime numbers
 ;============================================
 
-    MOV     EAX,  1                                 ; start checking by 1
-    MOV     ECX,  value                             ; for setting up outer loop counter upto 'value' times
+    MOV     EDX,  OFFSET  extracd1
+    CALL    WriteString
+    CALL    CrLF
+    CALL    CrLF
+
+    MOV     EAX,  1                                         ; start checking by 1
+    MOV     ECX,  value                                     ; for setting up outer loop counter upto 'value' times
     JMP     _checkPrime
 
     _checkPrime:
-    MOV     EBX,  0                                 ; 0: non-prime; 1: prime
+    MOV     EBX,  0                                         ; 0: non-prime; 1: prime
     CALL    isPrime
-    CMP     EBX,  1
+    CMP     EBX,  1                                         ; compare returned EBX from isPrime to 0 || 1
     JE      _displayPrime
     JMP     _advanceEAX
-	RET
 
     ; jump here if isPrime returns 0
     _displayPrime:
     CALL    WriteDec
-    MOV     EDX,  OFFSET  output1
+    CMP     EAX,  TEN
+    JL      _formatOutput1
+    JMP     _formatOutput2
+    
+    _resume:
     CALL    WriteString
     INC     EAX
-    INC     rowCounter
-    CMP     rowCounter,  MAX_ROW
+    INC     wordCount
+    CMP     wordCount,  MAX_ROW
     JE      _nextRow
     LOOP    _checkPrime
+    CALL    CrLF
     RET
 
     ; jump here if a row contains 10 values
     _nextRow:
     CALL    CrLF
-    MOV     rowCounter,  1
+    MOV     wordCount,  0
     LOOP    _checkPrime
     RET
 
+    ; jump here if the prime value < 10
+    _formatOutput1:
+    MOV     EDX,  OFFSET  output1
+    JMP     _resume
+
+    ; jump here if the prime value < 100
+    _formatOutput2:
+    MOV     EDX,  OFFSET  output2
+    CMP     EAX,  HUNDRED
+    JL      _resume
+    JMP     _formatOutput3
+
+    ; jump here if the prime value < 1000
+    _formatOutput3:
+    MOV     EDX,  OFFSET  output3
+    CMP     EAX,  THOUSAND
+    JL      _resume
+    JMP    _formatOutput4
+    
+    ; jump here if the prime value < 10000
+    _formatOutput4:
+    MOV     EDX,  OFFSET  output4
+    JMP     _resume
+
+    ; jump here to check the next integer (from 1, 2, 3++.... until displayed 'value' prime numbers)
     _advanceEAX:
     INC     EAX
     JMP    _checkPrime
@@ -215,15 +265,15 @@ isPrime PROC
 ;============================================
 
     CMP     EAX,  1
-    JE      _notPrimeSpeical
+    JE      _notPrimeSpecial
     CMP     EAX,  2
     JE      _primeSpecial
     CMP     EAX,  3
     JE      _primeSpecial
-    PUSH    ECX                                     ; push the outer loop counter on stack
-    MOV     ECX,  EAX                               ; start here at EAX = 4 or above
-    SUB     ECX,  2                                 ; internal loop should loop for dividend - 2 times (exclude 1 and itself)
-    MOV     primeNum,  2                            ; re-initialize for primeNum (divisor)
+    PUSH    ECX                                             ; push the outer loop counter on stack
+    MOV     ECX,  EAX                                       ; start here at EAX = 4 or above
+    SUB     ECX,  2                                         ; internal loop should loop for dividend - 2 times (exclude 1 and itself)
+    MOV     primeNum,  2                                    ; re-initialize for primeNum (divisor)
     JMP     _furtherCheck
 
     ; jump here if the value is exactly 1
@@ -234,8 +284,8 @@ isPrime PROC
     ; jump here if the value is not prime (other than 1)
     _notPrime:
     MOV     EBX,  0
-    POP     EAX
-    POP     ECX                                     ; restore the value of outer loop counter
+    POP     EAX                                             ; restore the value of dividend
+    POP     ECX                                             ; restore the value of outer loop counter
     RET
 
     ; jump here if the value is 2 || 3
@@ -251,14 +301,14 @@ isPrime PROC
 
     ; jump here if the value is 4 or above
     _furtherCheck:
-    PUSH    EAX                                     ; push the original dividend on stack
-    MOV     EBX,  primeNum                          ; set the divisor as primeNum
-    MOV     EDX,  0                                 ; set the remainder as 0
-    DIV     EBX                                     ; EAX here should start with 4 (as case 1-3 are tested)
+    PUSH    EAX                                             ; push the original dividend on stack
+    MOV     EBX,  primeNum                                  ; set the divisor as primeNum
+    MOV     EDX,  0                                         ; set the remainder as 0
+    DIV     EBX                                             ; EAX here should start with 4 (as case 1-3 are tested)
     CMP     EDX,  0
     JZ      _notPrime
-    POP     EAX                                     ; restore original EAX (dividend)
-    INC     primeNum                                ; increment divisor from 2 to dividend - 1 for checking
+    POP     EAX                                             ; restore original EAX (dividend)
+    INC     primeNum                                        ; increment divisor from 2 to dividend - 1 for checking
     LOOP    _furtherCheck
     JMP     _prime
 
@@ -274,9 +324,54 @@ farewell PROC
 ; returns: N/A
 ;============================================
 
-	CALL    CrLF
+    CALL    CrLF
 	MOV     EDX,  OFFSET  bye
 	CALL    WriteString
+    CALL    CrLF
+    CALL    CrLF
+    MOV     EDX,  OFFSET  bye1
+	CALL    WriteString
+    CALL    CrLF
+    CALL    CrLF
+    MOV     finished,  1                                    ; indicate the end of basic program (in range 1 - 200)
+    CALL    WaitMsg                                         ; intentionally pause the program to mark the start for the Extra Credit in range 1 - 4000
 	RET
 
 farewell ENDP
+
+;============================================
+getUserData2 PROC
+
+; To get an integer ranged between [1...4000]
+; preconditions: printing the instructions
+; postconditions: EDX changed (for WriteString); EAX changed (user data in and in range)
+; receives: N/A
+; returns: the validated data will be stored in global variables 'value'
+;============================================
+
+; jump here for asking input
+_askInput:
+    CALL    ClrScr
+    MOV     EDX,  OFFSET  prompt1
+    CALL    WriteString
+    MOV     EAX,  LOWER_BOUND
+    CALL    WriteDec
+    MOV     EDX,  OFFSET  prompt2
+    CALL    WriteString
+    MOV     EAX,  UPPER_BOUND2
+    CALL    WriteDec
+    MOV     EDX,  OFFSET  prompt3
+    CALL    WriteString
+
+    MOV     EAX,  0                                         ; initialize EAX as 0
+    CALL    ReadInt                                         ; ask for user input
+    CALL    CrLF
+
+    CALL    validate                                        ; call validate to check if within range
+    CMP     EBX,  1                                         ; returned EBX: if 0 (valid input); 1 (invalid input)
+    JE      _askInput                                       ; ask user's input again if EBX = 1
+    RET                                                     ; validate already move EAX into 'value' already
+
+getUserData2 ENDP
+
+END main
